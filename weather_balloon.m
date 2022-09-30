ead9842a09d5c150147bc6f1436da6ba6c9f73eb
mrobin <robin.malmborg97@gmail.com>
2022-09-30 19:45:46 +0200

    Add files via upload
    updated script for weather balloon, however no wind is simulated

---
+++ weather_balloon.m
@@ -0,0 +1,82 @@
+
+g = 9.81;                   %gravity
+mass = 2;                   %we assume weight is 2 kg
+vol = 4;                    %we assume volume is 4 m^3
+rho = 1.293;                %air density at sea level
+D = 3.7;                    %0.5*0.9*rho*(area/2);
+Fb = vol*rho*g;             %lifting force
+
+%wind
+%w0 = 7;
+%q = 10;
+
+%Euler-Cromer method
+time=2;                     %time interval
+dt=.01;                     %time-step, delta t
+
+%decides how many rows will be in each matrix based on delta t
+n = ceil(time/dt);
+
+
+a0 = zeros(n,1);
+
+%create and set matrices to n rows and 1 column
+y = zeros(n,1);             %y(position)
+v = zeros(n,1);             %v(velocity)
+a = zeros(n,1);             %a(acceleration)
+%ax = zeros(n,1);            %acceleration on x
+%w = zeros(n,1);             %wind
+%vx = zeros(n,1);            %velocity on x
+t = zeros(n,1);             %t(time)
+
+%set initial conditions
+a(1) = Fb/mass-g-(D/mass)*abs(v(1))*v(1);   %set acceleration on y
+%ax(1) = (D/mass)*abs(vx(1))*vx(1);          %set acceleration on x
+%w(1) = 5;                                  %initial wind velocity m/s
+a0(1) = Fb/mass;
+
+for i = 1:n-1
+    v(i+1) = v(i) + a(i)*dt;                        %integral of acceleration
+    y(i+1) = y(i) + v(i)*dt;                        %integral of velocity
+    a(i+1) = Fb/mass-g-(D/mass)*abs(v(i))*v(i);     %acceleration with drag
+    %ax(i+1)= (D/mass)*abs(vx(i))*vx(i);            %acceleration on x-axis
+    %w(i+1) = w0*(1-exp(-y(i)/q));                  %wind speed based on height
+    %vx(i+1) = vx(i) + ax(i)*dt;                    %velocity on x-axis
+    t(i+1) = t(i) + dt;                             %set time for each i
+end
+
+%plot displacement over time
+subplot(4,1,1)
+plot(t,y,'LineWidth',1)
+xlabel('Time (s)')
+ylabel('Displacement (m)')
+
+%plot velocity over time
+subplot(4,1,2)
+plot(t,v,'LineWidth',1)
+%set y-axis limit to 4, makes it easier to read graph
+ylim([0 4])
+xlabel('Time (s)')
+ylabel('Velocity (m/s)')
+%point when acceleration is close enough to 0 = terminal velocity
+hold on
+tv = plot(0.65,2.90001,'r*');
+legend(tv,'Terminal velocity')
+hold off
+
+%plot acceleration over time
+subplot(4,1,3)
+plot(t,a,'LineWidth',1)
+%set y-axis limit to 20, makes it easier to read graph
+ylim([0 20])
+xlabel('Time (s)')
+ylabel('Acceleration (m/s^2)')
+hold on
+tv = plot(0.65,0,'r*');
+legend(tv,'Terminal velocity')
+hold off
+
+subplot(4,1,4)
+%plot(t,a0)
+%xlabel('Time (s)')
+%ylabel('Acceleration (m/s)^2')
