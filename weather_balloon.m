e8152c2deec27419e0625ab4ab74b68934563c54
mrobin <robin.malmborg97@gmail.com>
2022-09-30 22:16:54 +0200

    added comparison plot of y and v

--- weather_balloon.m
+++ weather_balloon.m
@@ -17,9 +17,6 @@ dt=.01;                     %time-step, delta t
 %decides how many rows will be in each matrix based on delta t
 n = ceil(time/dt);
 
-
-a0 = zeros(n,1);
-
 %create and set matrices to n rows and 1 column
 y = zeros(n,1);             %y(position)
 v = zeros(n,1);             %v(velocity)
@@ -27,13 +24,19 @@ a = zeros(n,1);             %a(acceleration)
 %ax = zeros(n,1);            %acceleration on x
 %w = zeros(n,1);             %wind
 %vx = zeros(n,1);            %velocity on x
+
+%calculate a,v,y with no drag(Nd)
+aNd = zeros(n,1);           
+vNd = zeros(n,1);
+yNd = zeros(n,1);
+
 t = zeros(n,1);             %t(time)
 
 %set initial conditions
 a(1) = Fb/mass-g-(D/mass)*abs(v(1))*v(1);   %set acceleration on y
 %ax(1) = (D/mass)*abs(vx(1))*vx(1);          %set acceleration on x
 %w(1) = 5;                                  %initial wind velocity m/s
-a0(1) = Fb/mass;
+aNd = Fb/mass;
 
 for i = 1:n-1
     v(i+1) = v(i) + a(i)*dt;                        %integral of acceleration
@@ -42,6 +45,10 @@ for i = 1:n-1
     %ax(i+1)= (D/mass)*abs(vx(i))*vx(i);            %acceleration on x-axis
     %w(i+1) = w0*(1-exp(-y(i)/q));                  %wind speed based on height
     %vx(i+1) = vx(i) + ax(i)*dt;                    %velocity on x-axis
+    vNd(i+1) = vNd(i) + aNd(i)*dt;                        %integral of acceleration
+    yNd(i+1) = yNd(i) + vNd(i)*dt;  
+    aNd(i+1) = Fb/mass;
+
     t(i+1) = t(i) + dt;                             %set time for each i
 end
 
@@ -50,16 +57,20 @@ subplot(4,1,1)
 plot(t,y,'LineWidth',1)
 xlabel('Time (s)')
 ylabel('Displacement (m)')
+hold on
+plot(t,yNd, 'Linewidth', 1)
+hold off
 
 %plot velocity over time
 subplot(4,1,2)
 plot(t,v,'LineWidth',1)
 %set y-axis limit to 4, makes it easier to read graph
-ylim([0 4])
+ylim([0 10])
 xlabel('Time (s)')
 ylabel('Velocity (m/s)')
 %point when acceleration is close enough to 0 = terminal velocity
 hold on
+plot(t,vNd, 'Linewidth',1)
 tv = plot(0.65,2.90001,'r*');
 legend(tv,'Terminal velocity')
 hold off
@@ -76,7 +87,3 @@ tv = plot(0.65,0,'r*');
 legend(tv,'Terminal velocity')
 hold off
 
-subplot(4,1,4)
-%plot(t,a0)
-%xlabel('Time (s)')
-%ylabel('Acceleration (m/s)^2')
